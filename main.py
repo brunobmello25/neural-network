d0d0eaaf78e3c65a0b198bbf5de5579e94e9234e
Bruno Mello <bruno.barros.mello@gmail.com>
2025-02-10 23:49:15 -0300

    multiple inputs

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
-inputs = [1, 2, 3, 2.5]
-weights = [
+import numpy as np
+
+inputs = [  # 3x4
+    [1, 2, 3, 2.5],
+    [2, 5, -1, 2],
+    [-1.5, 2.7, 3.3, -0.8],
+]
+weights = [  # 3x4
     [0.2, 0.8, -0.5, 1],
     [0.5, -0.91, 0.26, -0.5],
     [-0.26, -0.27, 0.17, 0.87],
 ]
 biases = [2, 3, 0.5]
 
-layer_outputs = []
-for neuron_weights, neuron_bias in zip(weights, biases):
-    neuron_output = 0
+print('old:')
+print(np.dot(weights, inputs[0]) + biases)
+print()
 
-    for input, weight in zip(inputs, neuron_weights):
-        neuron_output += input * weight
-    neuron_output += neuron_bias
-
-    layer_outputs.append(neuron_output)
-
-print(layer_outputs)
+print('new:')
+output = np.dot(inputs, np.array(weights).T) + biases
+print(output)
+print()
